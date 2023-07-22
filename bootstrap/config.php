706cd7f36a7ab29192f05191ba2c3b02bf2fec97
Luis Felipe Palacio Salamanca <siulacio@hotmail.com>
2023-07-22 07:48:14 -0500

    database configuration file and load into container

--- bootstrap/config.php
+++ bootstrap/config.php
@@ -3,6 +3,9 @@
 use Application\Controllers\HomeController;
 
 return [
+    'config.database' => function () {
+        return parse_ini_file(base_path('app/Config/database.php'));
+    },
     HomeController::class => function () {
         return new HomeController;
     }
